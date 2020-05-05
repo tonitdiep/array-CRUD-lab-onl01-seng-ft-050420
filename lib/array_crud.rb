875ad4f0de7209c3248244f92ad4450a5237a746
Toni  Diep <adiep123@gmail.com>
2020-05-05 03:27:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,15 +7,15 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.shift
+  array.shift(element)
 end
 
 def remove_element_from_start_of_array(array)
